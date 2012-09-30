68a401a1472c460d32bbea9b26437990e914de88
Omar Ayub <omarayub@Omars-MacBook-Pro.local>
2012-09-29 21:46:59 -0400

    Initial commit of the service
    There isn't much here, just an example webservice that we'll use as a
    base line.

---
+++ DataLoggerService/DataLoggerService/server.js
@@ -0,0 +1,26 @@
+var restify = require('./node_modules/restify/');
+var server = restify.createServer();
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+}
+
+ function send(req, res, next) {
+   res.send('hello ' + req.params.name);
+   return next();
+ }
+
+ server.post('/hello', function create(req, res, next) {
+   res.send(201, Math.random().toString(36).substr(3, 8));
+   return next();
+ });
+ server.put('/hello', send);
+ server.get('/hello/:name', send);
+ server.head('/hello/:name', send);
+ server.del('hello/:name', function rm(req, res, next) {
+   res.send(204);
+   return next();
+ });
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
