72c1002c01b6dcc94a7953c78abc3f6d0ede8272
Omar Ayub <omarayub@Omars-MacBook-Pro.local>
2012-10-02 22:46:36 -0400

    Changed the support modules associated with this

--- DataLoggerService/DataLoggerService/server.js
+++ DataLoggerService/DataLoggerService/server.js
@@ -1,4 +1,5 @@
 var restify = require('./node_modules/restify/');
+var mongodb = require('./node_modules/mongoskin/');
 var server = restify.createServer();
 
 function respond(req, res, next) {
@@ -9,13 +10,24 @@ function respond(req, res, next) {
    res.send('hello ' + req.params.name);
    return next();
  }
+ 
+ function putName(req, res, next) {
+   res.send('puttings ' + req.params.name + '   ' + req.params.id);
+   return next();
+ }
+ 
+ function getName(req, res, next){
+     req.send('getting ' + req.params.name);
+     return next();
+ }
 
  server.post('/hello', function create(req, res, next) {
    res.send(201, Math.random().toString(36).substr(3, 8));
    return next();
  });
- server.put('/hello', send);
- server.get('/hello/:name', send);
+ 
+ server.put('/hello', getName);
+ server.get('/hello/:name/:id', putName);
  server.head('/hello/:name', send);
  server.del('hello/:name', function rm(req, res, next) {
    res.send(204);
